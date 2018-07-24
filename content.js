46313aaa74472cdda2906777e836dc366df12c2f
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-07-24 20:25:19 +0200

    Coloring visible links with green, not visible with red

--- content.js
+++ content.js
@@ -1,12 +1,25 @@
 /* jslint esversion: 6 */
 
+function modify_element_after_visibility(el) {
+    var rect = el.getBoundingClientRect();
+    // Completely visible
+    if ((rect.top >= 0) && (rect.bottom <= window.innerHeight) && (rect.left >= 0) && (rect.right <= window.innerWidth)) {
+        el.style.outline = "1px solid green";
+    // Partially visible
+    // } else if ((rect.top)) {
+    // Not at all visible
+    } else {
+        el.style.outline = "1px solid red";
+    }
+}
+
 console.log("Getting all links:");
 
 let links = document.querySelectorAll("a");
 let links_string = "";
 links.forEach(function(element, index) {
     links_string += "\n" + index + ": " + element.href;
-    element.style.outline = "1px solid red";
+    modify_element_after_visibility(element);
 });
 console.log("Links: " + links_string);
 console.log("Total links received: " + links.length);

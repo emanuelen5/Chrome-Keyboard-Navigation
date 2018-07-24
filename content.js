aeb4cc7ea220ba250e7c208bec5e16bb7c943c2b
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-07-24 20:10:02 +0200

    Adding a red border around all links

--- content.js
+++ content.js
@@ -1,1 +1,12 @@
-console.log("Awesome content run.....!@$%^&*()");
+/* jslint esversion: 6 */
+
+console.log("Getting all links:");
+
+let links = document.querySelectorAll("a");
+let links_string = "";
+links.forEach(function(element, index) {
+    links_string += "\n" + index + ": " + element.href;
+    element.style.outline = "1px solid red";
+});
+console.log("Links: " + links_string);
+console.log("Total links received: " + links.length);

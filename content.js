f5e39ac972264dc3242e3fd339fdaab15308245c
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-07-25 08:15:45 +0200

    Run link coloring when pressing key combination

--- content.js
+++ content.js
@@ -14,16 +14,27 @@ function modify_element_after_visibility(el) {
     }
 }
 
+function color_links() {
+    let links = document.querySelectorAll("a");
+    let links_string = "";
+    links.forEach(function(element, index) {
+        links_string += "\n" + index + ": " + element.href;
+        modify_element_after_visibility(element);
+    });
+    console.log("Links: " + links_string);
+    console.log("Total links received: " + links.length);
+}
+
+document.addEventListener("keydown", function make_links_visible(event) {
+    // Ctrl + Alt + e
+    if (event.key === "e" && event.ctrlKey && event.altKey) {
+        color_links();
+    }
+});
+
 console.log("Getting all links:");
 
 let start = new Date().getTime();
-let links = document.querySelectorAll("a");
-let links_string = "";
-links.forEach(function(element, index) {
-    links_string += "\n" + index + ": " + element.href;
-    modify_element_after_visibility(element);
-});
-console.log("Links: " + links_string);
-console.log("Total links received: " + links.length);
+color_links();
 let end = new Date().getTime();
 console.log("Total runtime: " + (end - start).toFixed(5));

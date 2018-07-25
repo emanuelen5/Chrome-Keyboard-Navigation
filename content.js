8b0a25c8a49835b07b51f8b14839b9dfd7cc2c57
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-07-25 08:32:38 +0200

    Only running coloring on keydown; creating wrapper for timing execution

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 /* jslint esversion: 6 */
 
-function modify_element_after_visibility(el) {
+function color_element_from_position(el) {
     var rect = el.getBoundingClientRect();
     // Completely visible
     if ((rect.top >= 0) && (rect.bottom <= window.innerHeight) && (rect.left >= 0) && (rect.right <= window.innerWidth)) {
@@ -15,26 +15,30 @@ function modify_element_after_visibility(el) {
 }
 
 function color_links() {
+    console.log("Getting all links:");
     let links = document.querySelectorAll("a");
     let links_string = "";
     links.forEach(function(element, index) {
         links_string += "\n" + index + ": " + element.href;
-        modify_element_after_visibility(element);
+        color_element_from_position(element);
     });
     console.log("Links: " + links_string);
     console.log("Total links received: " + links.length);
 }
 
+function timed_function_call(fn) {
+    fn_args = [];
+    for (var i = 1; i < arguments.length; i++) {
+        fn_args.push(arguments[i]);
+    }
+    let start = new Date().getTime();
+    fn(fn_args);
+    let end = new Date().getTime();
+    console.log("Total runtime: " + (end - start).toFixed(5));
+}
+
 document.addEventListener("keydown", function make_links_visible(event) {
-    // Ctrl + Alt + e
     if (event.key === "e" && event.ctrlKey && event.altKey) {
-        color_links();
+        timed_function_call(color_links);
     }
 });
-
-console.log("Getting all links:");
-
-let start = new Date().getTime();
-color_links();
-let end = new Date().getTime();
-console.log("Total runtime: " + (end - start).toFixed(5));

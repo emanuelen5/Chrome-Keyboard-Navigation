83d6d2d855ec80ade0ff1eb9f6587764f7b0ed32
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-09-16 16:03:06 +0200

    Adding chai package for testing

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
-var assert = require('assert');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
 
 describe('Dummy', function() {
     it('should always pass', function() {
         assert.equal(true, true);
+        expect(true).equal(true);
     });
 });

61173502e8ffdb41a8a586e5d045e17d0ca90cb6
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-09-23 20:06:48 +0200

    Wrong order of actual/expected

--- test/functions.js
+++ test/functions.js
@@ -13,39 +13,39 @@ describe('fuzzy_search', function() {
 
     it('should find word', function() {
         let search_result = fuzzy_search("hej", search_string);
-        assert.deepEqual([search_string, "h", "", "e", "ee", "j"], search_result);
+        assert.deepEqual(search_result, [search_string, "h", "", "e", "ee", "j"]);
     });
 
     it('first index should be total match', function() {
         let search_result = fuzzy_search("hej", search_string);
         let combined_strings = search_result.slice(1).join('');
-        assert.equal(search_result[0], combined_strings);
+        assert.equal(combined_strings, search_result[0]);
     });
 
     it('should find first letters of word', function() {
         let search_result = fuzzy_search("he", search_string);
-        assert.deepEqual(["he", "h", "", "e"], search_result);
+        assert.deepEqual(search_result, ["he", "h", "", "e"]);
     });
 
     it('should find letter', function() {
         let search_result = fuzzy_search("h", search_string);
-        assert.deepEqual(["h", "h"], search_result);
+        assert.deepEqual(search_result, ["h", "h"]);
     });
 
     it('should not find word', function() {
         let search_result = fuzzy_search("hejs", search_string);
-        assert.deepEqual([], search_result);
+        assert.deepEqual(search_result, []);
     });
 
     it('should not find letter', function() {
         let search_result = fuzzy_search("s", search_string);
-        assert.deepEqual([], search_result);
+        assert.deepEqual(search_result, []);
     });
 
     it('should match escaped strings', function() {
         search_string = "\\scan match as well\\s";
         let search_result = fuzzy_search("\\s\\s", search_string);
-        assert.deepEqual([search_string, '\\s', 'can match as well', '\\s'], search_result);
+        assert.deepEqual(search_result, [search_string, '\\s', 'can match as well', '\\s']);
     });
 });
 
@@ -53,17 +53,17 @@ describe('get_fuzzy_search_string', function() {
     it('should group letter', function() {
         let search_string = "a";
         let fuzzy_search_string = get_fuzzy_search_string(search_string);
-        assert.equal("(a)", fuzzy_search_string);
+        assert.equal(fuzzy_search_string, "(a)");
     });
 
     it('should group between letters', function() {
         let search_string = "aa";
         let fuzzy_search_string = get_fuzzy_search_string(search_string);
-        assert.equal("(a)(.*?)(a)", fuzzy_search_string);
+        assert.equal(fuzzy_search_string, "(a)(.*?)(a)");
     });
 
     it('should escape escapee as one group', function() {
         let fuzzy_search_string = get_fuzzy_search_string("\\");
-        assert.equal("(\\\\)", fuzzy_search_string);
+        assert.equal(fuzzy_search_string, "(\\\\)");
     });
 });

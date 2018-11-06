df54f1ad584750c6abcd5dfe5c0c66008b60da59
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-11-06 22:01:41 +0100

    Breaking out position classification into own function

--- content.js
+++ content.js
@@ -1,20 +1,39 @@
 /* global chrome */
 
-function color_element_from_position(el) {
+const VISIBLE = 0;
+const PARTIALLY_VISIBLE = 1;
+const OUTSIDE_FRAME = 2;
+
+function classify_position(el) {
     var rect = el.getBoundingClientRect();
+    // Completely visible
+    if ((rect.top >= 0) && (rect.bottom <= window.innerHeight) && (rect.left >= 0) && (rect.right <= window.innerWidth)) {
+        return VISIBLE;
+    // Partially visible
+    } else if (((rect.bottom > 0) && (rect.top < window.innerHeight)) && ((rect.right > 0) && (rect.left < window.innerWidth))) {
+        return PARTIALLY_VISIBLE;
+    // Not at all visible
+    } else {
+        return OUTSIDE_FRAME;
+    }
+}
+
+function color_element_from_position(el) {
     let classes = el.classList;
     ["kn__in_frame", "kn__outside_frame", "kn__partially_in_frame"].forEach(function(className) {
         classes.remove(className);
     });
-    // Completely visible
-    if ((rect.top >= 0) && (rect.bottom <= window.innerHeight) && (rect.left >= 0) && (rect.right <= window.innerWidth)) {
+    let pos_classification = classify_position(el);
+    switch (pos_classification) {
+    case VISIBLE:
         classes.add("kn__in_frame");
-    // Partially visible
-    } else if (((rect.bottom > 0) && (rect.top < window.innerHeight)) && ((rect.right > 0) && (rect.left < window.innerWidth))) {
+        break;
+    case PARTIALLY_VISIBLE:
         classes.add("kn__partially_in_frame");
-    // Not at all visible
-    } else {
+        break;
+    case OUTSIDE_FRAME:
         classes.add("kn__outside_frame");
+        break;
     }
 }
 
@@ -44,7 +63,7 @@ function timed_decorator(fn) {
 
 let color_links_with_timing = timed_decorator(color_links);
 document.addEventListener("keydown", function make_links_visible(event) {
-    if (event.key === "e" && event.ctrlKey && event.altKey) {
+    if (event.key === "g" && event.ctrlKey && event.altKey) {
         color_links_with_timing();
     }
 });
